0fd52c170d6989c0ba1132a53a8f198e104fcfcf
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-11-16 22:07:07 +0100

    update

--- components/Modals/ProductModals.tsx
+++ components/Modals/ProductModals.tsx
@@ -14,10 +14,14 @@ const ProductModals = () => {
     <>
       {isProductModalOpen && (
         <div
-          {...handlers}
+          onClick={closeProductModal}
           className="fixed top-0 bottom-0 z-30 right-0 left-0 bg-black/60 flex items-end"
         >
-          <div className="h-[90%] bg-white w-[100vw] rounded-3xl relative">
+          <div
+            {...handlers}
+            onClick={(e) => e.stopPropagation}
+            className="h-[90%] bg-white w-[100vw] rounded-3xl relative"
+          >
             <div className="w-16 h-2 rounded-full bg-zinc-200 absolute top-4 left-[50%] translate-x-[-50%]" />
           </div>
         </div>
