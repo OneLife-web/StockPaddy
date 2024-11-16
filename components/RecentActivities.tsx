b95d7ea8f9494eb8bee8c6c96780215a38080178
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-11-16 19:05:27 +0100

    update

--- components/RecentActivities.tsx
+++ components/RecentActivities.tsx
@@ -30,8 +30,7 @@ const AuditTrail = () => {
   ];
 
   return (
-    <div className="bg-white shadow-md rounded-lg p-4">
-      <h2 className="text-lg font-semibold mb-4">Recent Activities</h2>
+    <div className="px-3">
       <ul className="space-y-3">
         {auditTrail.map((item, index) => (
           <li
