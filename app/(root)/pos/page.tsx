bab7b5f9b20f894b3c223325092b499a37c4abed
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-11-18 01:57:22 +0100

    update

--- app/(root)/pos/page.tsx
+++ app/(root)/pos/page.tsx
@@ -1,15 +1,19 @@
 "use client";
 import BarcodeScanner from "@/components/BarCode/BarCodeScanner";
+import { useState } from "react";
 
 const POSPage = () => {
+  const [barCodeData, setBarCodeData] = useState("");
   const handleScan = (data: string) => {
     console.log("Scanned data:", data);
+    setBarCodeData(data);
     // You can add additional logic here to handle the scanned data
   };
   return (
-    <div>
+    <div className="pageContainer">
       <h1>My POS</h1>
       <BarcodeScanner onScan={handleScan} />
+      <p>{barCodeData}</p>
     </div>
   );
 };
